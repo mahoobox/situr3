b2ea8ab9db70408e2cfe01306646f10c059f64b5
mahoobox <mauricio.morales@somosargus.com>
2017-06-27 18:02:31 -0500

    mejro así

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-"""    query = data.get('query')
-    if query is None:
+    query = data.get('query')
+"""    if query is None:
         return {}
 
     result = query.get('results')
