eb3576843614de220eec694297f1be9c865f89d7
mahoobox <mauricio.morales@somosargus.com>
2017-06-27 11:52:24 -0500

    restaurando original

--- app.py
+++ app.py
@@ -84,9 +84,8 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-"""    speech = "Hoy Mauricio in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')"""
-    speech = "Confirmo que si encontré lo que buscaba " + location.get('city')
+    speech = "Hoy Mauricio in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
