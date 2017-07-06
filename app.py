c7c92c9fb424d512ae01ec258d55184bbd0cf297
mahoobox <mauricio.morales@somosargus.com>
2017-07-06 15:22:59 -0500

    retorno de datos

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "buscarAtractivos":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "http://situr.boyaca.gov.co/wp-json/wp/v2/atractivo_turistico?search="
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    yql_url = baseurl + yql_query
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    res = city2
+    res = makeWebhookResult(data)
     return res
 
 
@@ -59,7 +59,7 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return city2
+    return city
 
 
 def makeWebhookResult(data):
