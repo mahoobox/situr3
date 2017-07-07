8becfc57461495287b0c6b21a234821e26fca0de
mahoobox <mauricio.morales@somosargus.com>
2017-07-07 09:17:56 -0500

    retorno de nuevo

--- app.py
+++ app.py
@@ -18,31 +18,101 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
-buscasitur = "parque solano"
-urlsitur = "http://situr.boyaca.gov.co/wp-json/wp/v2/atractivo_turistico?search="
-buscasitur_sin_espacio = buscasitur.replace(" ", "%20")
-leer = json.loads(urlopen(urlsitur + buscasitur_sin_espacio).read())
-nombre_atractivo = leer[0]['title']['rendered']
-descripcion_atractivo = leer[0]['excerpt']['rendered']
-url_atractivo = leer[0].get('link')
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "buscarAtractivos":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    global city
+    global city2
+    city = parameters.get("atractivos")
+    city2 = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    if city is None:
+        return None
+
+    return city2
+
+
+def makeWebhookResult(data):
+    buscasitur = city
+    urlsitur = "http://situr.boyaca.gov.co/wp-json/wp/v2/atractivo_turistico?search="
+    buscasitur_sin_espacio = buscasitur.replace(" ", "%20")
+    leer = json.loads(urlopen(urlsitur + buscasitur_sin_espacio).read())
+    test = leer[0].get('slug')
+    nombre_atractivo = leer[0]['title']['rendered']
+    descripcion_atractivo = leer[0]['excerpt']['rendered']
+    url_atractivo = leer[0].get('link')
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    mahoobox = " hola mundo dato ingresado: "
+    datoapi = (json.dumps(item, indent=4))
 
     # print(json.dumps(item, indent=4))
-#    speech = "Hoy Mauricio in " + location.get('city') + ": " + condition.get('text') + ", SI ENTENDIO LA TEMPERATURA " + condition.get('temp') + " " + units.get('temperature')
-speech = "Mira, encontré esta información sobre  " + nombre_atractivo + ": " + descripcion_atractivo + "        Si quieres ver más info visita:   "  + url_atractivo
 
-print("Response:")
-print(speech)
+#    speech = "Hoy Mauricio in " + location.get('city') + ": " + condition.get('text') + ", SI ENTENDIO LA TEMPERATURA " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Mira, encontré esta información sobre  " + nombre_atractivo + ": " + descripcion_atractivo + "        Si quieres ver más info visita:  "  + url_atractivo + city
+
+    print("Response:")
+    print(speech)
   #  print(salidapruebas)
 
-return {
-    "speech": speech,
+    return {
+        "speech": speech,
  #       "salidapruebas": salidapruebas
-    "displayText": speech,
-       # "data": data,
-       # "contextOut": [],
-    "source": "apiai-weather-webhook-sample"
-}
+        "displayText": speech,
+        "data": data,
+        "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
 
 
 if __name__ == '__main__':
