9594311542a6648e2a5d78fa847c52c8a8ed8bf2
mahoobox <mauricio.morales@somosargus.com>
2017-07-07 10:31:31 -0500

    resumiendo consulta

--- app.py
+++ app.py
@@ -26,95 +26,36 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)#DIFERENCIA 1
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
+def makeWebhookResult(req):
     if req.get("result").get("action") != "buscarAtractivos":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    global city
-    global city2
-    city = parameters.get("atractivos")
-    city2 = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-    if city is None:
-        return None
+    zone = parameters.get("atractivos")
 
-    return city2
+    cost = {'parque':100, 'casa':200, 'carro':300, 'reloj':400}
 
-
-def makeWebhookResult(data):
-    buscasitur = city
-    urlsitur = "http://situr.boyaca.gov.co/wp-json/wp/v2/atractivo_turistico?search="
-    buscasitur_sin_espacio = buscasitur.replace(" ", "%20")
-    leer = json.loads(urlopen(urlsitur + buscasitur_sin_espacio).read())
-    test = leer[0].get('slug')
-    nombre_atractivo = leer[0]['title']['rendered']
-    descripcion_atractivo = leer[0]['excerpt']['rendered']
-    url_atractivo = leer[0].get('link')
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    mahoobox = " hola mundo dato ingresado: "
-    datoapi = (json.dumps(item, indent=4))
-
-    # print(json.dumps(item, indent=4))
-
-#    speech = "Hoy Mauricio in " + location.get('city') + ": " + condition.get('text') + ", SI ENTENDIO LA TEMPERATURA " + condition.get('temp') + " " + units.get('temperature')
-    speech = "Mira, encontré esta información sobre  " + nombre_atractivo + ": " + descripcion_atractivo + "        Si quieres ver más info visita:  "  + url_atractivo + city
+    speech = "El elemento que solicitaste es: " + zone + " y su valor asignado es " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
-  #  print(salidapruebas)
 
     return {
         "speech": speech,
- #       "salidapruebas": salidapruebas
         "displayText": speech,
-        "data": data,
-        "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
     }
-
-
+    
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
