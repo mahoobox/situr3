a9f8960ebd8138940a8817c7dce49d9bbd3124f8
mahoobox <mauricio.morales@somosargus.com>
2017-08-03 18:01:58 -0500

    restore

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def makeWebhookResult(req):
             },"""
         "contextOut": [{"name":"desdepython", "lifespan":1}],
  #       "contextOut": [],
-        "source": '"'listadoBusqueda(leerAtractivo)'"'
+        "source": listadoBusqueda(leerAtractivo)
     }
     
 if __name__ == '__main__':
