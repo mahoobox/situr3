bc05821c97e76b5e0508970a82ed8440e253fdec
mahoobox <mauricio.morales@somosargus.com>
2017-08-03 18:11:56 -0500

    ultimacontexto

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def makeWebhookResult(req):
                 }
             },"""
 #        "contextOut": [],
-        "contextOut": [{"name":"desdepython", "lifespan":1}],
+        "contextOut": [{"name":"desdepython", "lifespan":2}],
         "source": "soy-un-dato-irrelevante"
 #        "source": listadoBusqueda(leerAtractivo)
     }
