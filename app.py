d247b04600bbc0bd077e2629600c7d5b67e65589
mahoobox <mauricio.morales@somosargus.com>
2017-08-04 11:23:33 -0500

    coma

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def makeWebhookResult(req):
         "data" : {  
                 "facebook":{  
                     "text":"soy un texto, y si funciono"
-                }
+                },
                 "facebook" : {
                     "attachment" : {
                         "type" : "template",
