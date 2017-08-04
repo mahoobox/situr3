e241b8567e20b7337c015277e69a448cf5bbbbb3
mahoobox <mauricio.morales@somosargus.com>
2017-08-04 10:17:48 -0500

    sdf

--- app.py
+++ app.py
@@ -148,68 +148,6 @@ def makeWebhookResult(req):
                 }
             }
         },
-"""        "data" : {
-                "facebook" : {
-                    "attachment" : {
-                        "type" : "template",
-                        "payload" : {
-                            "template_type" : "generic",
-                            "elements" : [
-                                {
-                                    "title" : tituloAtractivo,
-                                    "image_url" : imagenAtractivo,
-                                    "subtitle": descripcionAtractivo,
-                                    "buttons":  [
-                                        {
-                                            "type":"web_url",
-                                            "url": "http://situr.boyaca.gov.co",
-                                            "title": "Ver"
-                                        },
-                                        {
-                                             "type":"web_url",
-                                            "url": "http://situr.boyaca.gov.co",
-                                            "title": "Ver2"
-                                        },
-                                        {
-                                              "type":"web_url",
-                                            "url": "http://situr.boyaca.gov.co",
-                                            "title": "Ver3"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "title": tituloAtractivo,
-                                    "image_url": imagenAtractivo,
-                                    "subtitle": descripcionAtractivo,
-                                    "default_action": {
-                                        "type": "web_url",
-                                        "url": "https://www.moovrika.com/m/4167",
-                                        "webview_height_ratio": "tall"
-                                    },
-                                    "buttons": [
-                                        {
-                                            "title": "more info",
-                                            "type": "web_url",
-                                            "url": "https://www.moovrika.com/m/4082",
-                                            "webview_height_ratio": "tall"
-                                        },
-                                        {
-                                             "type":"web_url",
-                                            "url": "http://situr.boyaca.gov.co",
-                                            "title": "Ver2"
-                                        },
-                                        {
-                                              "type":"web_url",
-                                            "url": "http://situr.boyaca.gov.co",
-                                            "title": "Ver3"
-                                        }
-                                    ]
-                                }
-                           ]
-                       }
-                    }
-                }
-            },"""
 #        "contextOut": [],
         "contextOut": [{"name":"desdepython", "lifespan":2}],
         "source": "soy-un-dato-irrelevante"
