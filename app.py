495529430ceae8847a6f5842d90349a73d1ab0b2
mahoobox <mauricio.morales@somosargus.com>
2017-08-05 17:29:33 -0500

    asfsdf

--- app.py
+++ app.py
@@ -37,15 +37,13 @@ def webhook():
     return r
 
 def listadoBusqueda(urlBaseJson, urlBaseImagen):
-    def inicioFuncion():
-        return inicioFBCard
-    inicioFuncion()
-    def dentroFuncion():
-        for x in range(0,len(urlBaseJson)):
+    print (inicioFBCard)
+    for x in range(0,len(urlBaseJson)):
 #        descripcionItem = re.sub("<.*?>", "", urlBaseJson[x]['excerpt']['rendered'])#Descripción del atractivo eliminando etiquetas
 #        idImgFichaAtrFB = str(urlBaseJson[x]['featured_media'])#ID de la imagen del atractivo
 #        leerImagenAtractivos = json.loads(urlopen(urlBaseImagen + idImgFichaAtrFB).read())#Une la URL base de las imágenes con el ID de imagen y lo lee como JSON
 #        imagenDefAtractivos = leerImagenAtractivos['media_details']['sizes']['medium']['source_url']#Interpreta el JSON de la imagen y extrae la URL de la imagen
+
         return ("""                            {
                                 "title" : "hola soy tu puto titulo",
                                 "image_url" : "https://www.dondevive.org/wp-content/uploads/2015/08/donde-viven-los-conejos.jpg",
@@ -62,8 +60,7 @@ def listadoBusqueda(urlBaseJson, urlBaseImagen):
                                         "title": "boton2"
                                     }
                                 ]
-                            }""")
-    dentroFuncion()
+                            },""")
     return finFBCard
 
 inicioFBCard = """{
@@ -170,7 +167,7 @@ def makeWebhookResult(req):
     leerImagenAtr = json.loads(urlopen(baseUrlImgAtract + idImagenAtractivo).read())
     imagenAtractivo = leerImagenAtr['media_details']['sizes']['medium']['source_url']
 
-    speech = "Ze encontrado " + cantidadResultados + " Resultados .   El atractivo que solicitaste es: " + tituloAtractivo + "  y la url de la imagen es: " + imagenAtractivo
+    speech = "Re encontrado " + cantidadResultados + " Resultados .   El atractivo que solicitaste es: " + tituloAtractivo + "  y la url de la imagen es: " + imagenAtractivo
     fbMsg = {
             "facebook" : {
 #                "text":{
@@ -243,8 +240,8 @@ def makeWebhookResult(req):
     return {
         "speech": speech,
         "displayText": speech,
-        "data" :listadoBusqueda(leerAtractivo, baseUrlImgAtract),
-#        "data" :mostrarFB(),
+#        "data" :listadoBusqueda(leerAtractivo, baseUrlImgAtract),
+        "data" :mostrarFB(),
 #        "contextOut": [],
         "contextOut": [{"name":"desdepython", "lifespan":2}],
         "source": "soy-un-dato-irrelevante"
