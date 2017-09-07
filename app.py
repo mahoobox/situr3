39cb72d33a32350817afc3c30044b8ad85027ed7
mahoobox <mauricio.morales@somosargus.com>
2017-09-07 17:10:00 -0500

    sfsdf

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def makeWebhookResult(req):
         #URL BASE CONSULTA ATRACTIVOS JSON
         baseUrl = "http://situr.boyaca.gov.co/wp-json/wp/v2/atractivo_turistico?per_page=10&orderby=relevance&search="#URL Base Atractivos
 
-        speech = " atractivos turísticos en la ciudad"
+        speech = " para este tractivo en la ciudad"
 
 
 
