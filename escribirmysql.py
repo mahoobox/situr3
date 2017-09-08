dbe8191978b06c04b94bff2a7ab91c1dd4e3e6df
mahoobox <mauricio.morales@somosargus.com>
2017-09-08 16:43:40 -0500

    sdffd

---
+++ escribirmysql.py
@@ -0,0 +1,24 @@
+import MySQLdb
+ 
+DB_HOST = '192.95.22.65' 
+DB_USER = 'sitursit_bot' 
+DB_PASS = 'RwfMXSUurWCX' 
+DB_NAME = 'sitursit_bot' 
+ 
+def run_query(query=''): 
+    datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME] 
+ 
+    conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
+    cursor = conn.cursor()         # Crear un cursor 
+    cursor.execute(query)          # Ejecutar una consulta 
+ 
+    if query.upper().startswith('SELECT'): 
+        data = cursor.fetchall()   # Traer los resultados de un select 
+    else: 
+        conn.commit()              # Hacer efectiva la escritura de datos 
+        data = None 
+ 
+    cursor.close()                 # Cerrar el cursor 
+    conn.close()                   # Cerrar la conexión 
+ 
+    return data
