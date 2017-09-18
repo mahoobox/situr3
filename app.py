45fa291630f6a7a28bcbbeaedec622edceee89b2
mahoobox <mauricio.morales@somosargus.com>
2017-09-18 16:38:06 -0500

    sdfd

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def makeWebhookResult(req):
         {
           "type": 2,
           "platform": "facebook",
-          "title": "",
+          "title": "¿Necesitas ayuda?",
           "replies": [
             "obtener ayuda"
           ]
